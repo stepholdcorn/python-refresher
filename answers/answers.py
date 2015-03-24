d27be42992c31ce27a82770af5ccb7327fd65c8c
steph <stephieoldcorn@gmail.com>
2015-03-24 13:54:21 +0000

    Selects elements starting with a vowel and removes nils

--- answers/answers.py
+++ answers/answers.py
@@ -3,3 +3,17 @@ def select_elements_starting_with_a(my_list):
   for item in my_list:
     if item.startswith('a'): result.append(item)
   return result
+
+def select_elements_starting_with_vowel(my_list):
+  result = []
+  vowel = list('aeiou')
+  for item in my_list:
+    for letter in vowel:
+      if item.startswith(letter): result.append(item)
+  return result
+
+def remove_nils_from_array(my_list):
+  result = []
+  for item in my_list:
+    if item != None: result.append(item)
+  return result
--- tests/answers_tests.py
+++ tests/answers_tests.py
@@ -3,3 +3,9 @@ from answers import answers
 
 def test_select_elements_starting_with_a():
   assert_equal(answers.select_elements_starting_with_a(['bananas', 'apples', 'pears', 'avocados']), ['apples', 'avocados'])
+
+def test_select_elements_starting_with_vowel():
+  assert_equal(answers.select_elements_starting_with_vowel(['john', 'david', 'omar', 'fred', 'idris', 'angela']), ['omar', 'idris', 'angela'])
+
+def test_remove_nils_from_array():
+  assert_equal(answers.remove_nils_from_array(['a', 'b', None, None, False, 'c', None]), ['a', 'b', False, 'c'])
